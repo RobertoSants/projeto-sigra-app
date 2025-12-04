379c8b17992918dd7f7be7e8113f6477e9cef18d
RobertoSants <j-uni-nho25@hotmail.com>
2025-12-04 08:59:05 -0300

    Adicionado seleção dinâmica de técnicos na manutenção.

--- src/controllers/manutencao-controller.js
+++ src/controllers/manutencao-controller.js
@@ -1,33 +1,33 @@
 const Manutencao = require('../models/manutencao');
 const Rastreador = require('../models/rastreador');
+const Usuario = require('../models/usuario'); // [NOVO] Importamos Usuario
 
 module.exports = {
     // Exibe a tela (Boundary)
     async exibirTela(req, res) {
         const rastreadores = await Rastreador.findAll();
-        res.render('tela-registrar-manutencao', { rastreadores });
+        
+        // [NOVO] Busca apenas usuários que são Técnicos para preencher o select
+        const tecnicos = await Usuario.findAll({ where: { cargo: 'Tecnico' } });
+
+        // Envia rastreadores E técnicos para a tela
+        res.render('tela-registrar-manutencao', { rastreadores, tecnicos });
     },
 
-    // [Diagrama de Sequência CSU03] Método Principal
+    // [Diagrama de Sequência CSU03] Registrar Manutenção
     async registrarManutencao(req, res) {
         const { rastreadorId, descricao, tecnico } = req.body;
 
         try {
-            // 1. [Diagrama de Sequência] Ação: "Registrar manutenção"
-            await Manutencao.create({
-                descricao,
-                tecnico,
-                rastreadorId
-            });
+            // 1. Grava histórico do defeito
+            await Manutencao.create({ descricao, tecnico, rastreadorId });
 
-            // 2. [Diagrama de Sequência] Ação: "Atualizar status"
-            // [RN04] Muda status para 'Em Manutenção'
+            // 2. Atualiza status para 'Em Manutenção'
             await Rastreador.update(
                 { status: 'Em Manutenção' },
                 { where: { id: rastreadorId } }
             );
 
-            // 3. Retorno de sucesso
             res.redirect('/');
         } catch (error) {
             res.send("Erro ao registrar manutenção.");
